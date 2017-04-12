577842eb79db55784c180db9d399e457b3771a3f
Jess <geiger117@live.missouristate.edu>
2017-04-11 20:37:27 -0500

    First set of navbar updates - working page nesting, still needs styling

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { browserHistory } from 'react-router';
+//import SearchPage from './pages/SearchPage.js';
 
 
 export default class HomePage extends React.Component {
@@ -22,13 +23,17 @@ export default class HomePage extends React.Component {
 
     return(
       <div>
-        <h1>Search</h1>
-        <form>
-          <label htmlFor="search">Search a user or game:</label><br />
-          <input id="searchBar" type="text" />
-          <button id="searchButton" type="Submit">Submit</button>
-        </form>
-      </div>
+      {this.props.children}
+        <div>
+          <h1>Search</h1>
+          <form>
+            <label htmlFor="search">Search a user or game:</label><br />
+            <input id="searchBar" type="text" />
+            <button id="searchButton" type="Submit">Submit</button>
+          </form>
+          <span id="searchResults">No results were found.</span>
+        </div>
+        </div>
     );
   }
 }
