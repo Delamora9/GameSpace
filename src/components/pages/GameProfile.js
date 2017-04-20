03d9cb833704ed415fc8cca3d02571e10f297744
Christopher Franklyn <chris4@live.missouristate.edu>
2017-04-20 15:33:41 -0500

    Started API calls and data collection for an individual game. Will continue to flesh out more calls for data and populate the page with it.

--- src/components/pages/GameProfile.js
+++ src/components/pages/GameProfile.js
@@ -1,13 +1,44 @@
 import React from 'react';
+const Steam = require('steam-webapi');
+
+// Set global Steam API Key
+Steam.key = "36991C4777F98B19F85825A2368DE13A";
 
 export default class GameProfile extends React.Component {
   render() {
     console.log(this.props);
     const {params} = this.props
+    Steam.ready(function(err) {
+      if (err) return console.log(err);
+      else {
+        const steam = new Steam();
+
+        //Retrieve list of Apps and their ID
+        steam.getAppList({}, function(err, data) {
+          //console.log(data);
+          let gameList = data['applist']['apps'];
+          let gameID;
+          for (let i = 0; i < gameList.length; i++) {
+            if (gameList[i].name == params.game) {
+              gameID = gameList[i].appid;
+            }
+          }
+          //console.log(gameID);
+
+          //Retrieve recent news for specific game
+          steam.getNewsForApp({appid: gameID, count: 5}, function(err, data){
+            //console.log(data);
+          });
+        });
+      }
+    });
+
     return(
       <div>
         <h1>Game Profile for {params.game}</h1>
-        <p>data</p>
+        <div id="divbody">
+
+        </div>
       </div>
     );
   }
