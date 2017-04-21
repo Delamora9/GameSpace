38ef32c043f5373f0d242fc1420784b54e97c084
Christopher Franklyn <Chris4@live.missouristate.edu>
2017-04-21 16:24:18 -0500

    Made unit-testing exclusive to karma.

--- test/unit/SearchPageSpec.js
+++ test/unit/SearchPageSpec.js
@@ -1,5 +1,8 @@
 import SearchPage from '../../src/components/pages/SearchPage.js';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+
 
 describe('Search page', () => {
 	beforeEach(function() {
