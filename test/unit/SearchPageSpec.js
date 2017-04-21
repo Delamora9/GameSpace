21d67ca60cfc2dff012940f81f17081b33d6fe9a
River Ginther <river.ginther@banno.com>
2017-04-21 13:44:40 -0500

    Simple fix. to text.

--- test/unit/SearchPageSpec.js
+++ test/unit/SearchPageSpec.js
@@ -5,7 +5,7 @@ describe('Search page', () => {
 		this.renderedDOM = () => React.findDOMNode(this.component);
 	});
 
-	it("renders a paragraph which greets someone", function() {
+	it("renders some JSX", function() {
 		expect(this.renderedDOM().children.length).toEqual(1);
 	});
 });
