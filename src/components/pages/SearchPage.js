3274921f294a64a90cf0c6db18ba709c83d160d8
KeithCissell <keithacissell@gmail.com>
2017-04-24 22:59:06 -0500

    Moved SearchPage API call into a function. Searching now returns single result if valid.

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -4,7 +4,6 @@ import { hashHistory } from 'react-router';
 
 export default class SearchPage extends React.Component {
   render() {
-
     return(
       <div>
         <h1>Search</h1>
@@ -13,23 +12,20 @@ export default class SearchPage extends React.Component {
           <input id="searchBar" type="text" />
           <button id="searchButton" type="Submit">Submit</button>
         </form>
-        <span id="searchResults">
-          No results were found.
-        </span>
         <ul id="returnResults"></ul>
       </div>
     );
  }
 
-
   componentDidMount() {
-    //capture DOM elements
+    // Capture DOM elements
+    let searchBar = document.getElementById("searchBar");
     let searchButton = document.getElementById("searchButton");
     if(searchButton){
         searchButton.addEventListener('click', newQuery, false);
     }
 
-    //function to update the page with a new search query
+    // Function to update the page with a new search query
     function newQuery() {
       let searchValue = searchBar.value;
       if (searchValue) {
@@ -38,51 +34,56 @@ export default class SearchPage extends React.Component {
         hashHistory.push(newPath);
       }
     }
-    //force componentDidUpdate to be called when SearchPage is first mounted
-    this.forceUpdate();
 
+    // Force componentDidUpdate to be called when SearchPage is first mounted
+    this.forceUpdate();
   }
 
   componentDidUpdate() {
-    let searchBar = document.getElementById("searchBar");
-    let searchResults = document.getElementById("searchResults");
-    //set cursor focus to searchBar
-    if(searchBar){
-      searchBar.focus();
-    }
-    document.getElementById("returnResults").innerHTML = "";
+    // Capture DOM elements
+    let searchResults = document.getElementById("returnResults");
+
+    // Get search value from URL query and search Steam
     if(this.props.location){
-      const { query } = this.props.location;
-      const { search } = query;
-      var Steam = require('steam-webapi');
+      let { query } = this.props.location;
+      let { search } = query;
 
-      Steam.key = "36991C4777F98B19F85825A2368DE13A";
-
-
-      //update the page to display the query's results
       if (search) {
-        //refill search bar value
         searchBar.value = search;
-        searchResults.innerHTML = "You searched: " + search;
-        Steam.ready(function(err) {
-            if (err) return console.log(err);
-
-            var steam = new Steam();
-
-            // Retrieve the steam ID from a steam username/communityID
-            steam.resolveVanityURL({vanityurl: search}, function(err, data) {
-                console.log(data);
-                });
-
-          });
-        var resultli = document.createElement('li');
-        var aTag = document.createElement('a');
-        aTag.setAttribute('href', "/#/user/" + search);
-        aTag.innerHTML = search;
-        resultli.appendChild(aTag);
-        document.getElementById("returnResults").appendChild(resultli);
+        searchBar.focus();
+        searchSteam(search);
       }
     }
+  
+    // Display result from Steam API
+    function searchSteam(search) {
+      searchResults.innerHTML = "Loading...";
 
-  }
-}
+      let Steam = require('steam-webapi');
+      Steam.key = "36991C4777F98B19F85825A2368DE13A";
+
+      // Update the page to display the query's results  
+      Steam.ready(function(err) {
+        if (err) return console.log(err);
+
+        let steam = new Steam();
+
+        // Retrieve the steam ID from a steam username/communityID
+        steam.resolveVanityURL({vanityurl: search}, function(err, data) {
+          // Display results
+          if (data.success == 1) {
+            let resultli = document.createElement('li');
+            let aTag = document.createElement('a');
+            aTag.setAttribute('href', "/#/user/" + search);
+            aTag.innerHTML = search;
+            resultli.appendChild(aTag);
+            searchResults.innerText = "";
+            searchResults.appendChild(resultli);
+          } else { searchResults.innerHTML = data.message; }
+        });
+      });
+    }
+
+  }//end componentDidUpdate
+
+}//end SearchPage
